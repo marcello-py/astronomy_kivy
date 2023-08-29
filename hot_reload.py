2dd7f058a9769fabae0fce311a99a8d61b5dd385
Marcelo <marcello_mn@hotmail.com>
2023-08-28 23:27:21 -0300

    uptdate

--- hot_reload.py
+++ hot_reload.py
@@ -1,15 +1,29 @@
 from kivymd.tools.hotreload.app import MDApp
 from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager,Screen
 from kivy.core.window import Window
 
+class FirstScreen(Screen):
+   pass
 
+
+class NewScreen(Screen):
+    pass
+
+sm = ScreenManager()
+
+# Adicionando as telas ao ScreenManager
+sm.add_widget(NewScreen(name = 'register'))
+sm.add_widget(FirstScreen(name = 'login'))
 
 class HotReload(MDApp):
     KV_FILES = ['app/Test.kv']
     DEBUG = True
+
     def build_app(self):
         return Builder.load_file('app/Test.kv')
     
+
 if __name__ == '__main__':
+    Window.size = (600, 1000)
     HotReload().run()
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from kivymd.app import MDApp
 from kivy.core.window import Window
+from kivy.lang import Builder
 
 class Test(MDApp):
-    pass
+    def build_app(self):
+        return Builder.load_file('app/Test.kv')
 
 if __name__ == '__main__':
     Window.size = (600, 1000)
