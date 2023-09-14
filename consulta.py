820c6104ab86fae13b13ca2ead96a9c16ddec902
Marcelo <marcello_mn@hotmail.com>
2023-09-14 00:45:35 -0300

    update

--- consulta.py
+++
@@ -1,7 +0,0 @@
-# Exemplo de queries.py
-from database import Database
-def select_users_by_name(name):
-    return f"SELECT * FROM users WHERE name = '{name}';"
-
-def insert_user(name, age):
-    return f"INSERT INTO users (name, age) VALUES ('{name}', {age});"
