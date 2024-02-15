3698e57bdb9ab1ba9281e8757816c1f1a4460b6b
Marcelo Nunes <marcello_mn@hotmail.com>
2024-02-14 23:12:14 -0300

    update\modify

---
+++ image/get_image.py
@@ -0,0 +1,36 @@
+import os
+
+class Image:
+    
+    def get(datas):
+        try:
+            dados = f'../APOC/{datas}.jpg'
+            if os.path.exists(dados): # Verificar se o arquivo existe dentro da pasta
+                print('arquivo existe')
+                with open(dados, 'rb') as file: # Abri o arquivo em binário
+                    imagem = file.read()  # Lê o conteúdo do arquivo da imagem
+                    return imagem  # Retorna a imagem
+            else:
+                print(f'o arquivo {datas}, não foi localizado.')
+                
+        except FileNotFoundError:
+            print(f'o arquivo {datas}, não foi localizado.')
+
+            
+    def verificar_caminho(caminho_arquivo):
+        # Abre o arquivo usando o aplicativo padrão do sistema operacional
+        os.system(f'start {caminho_arquivo}')
+            
+
+            
+    ano = '16'
+    mes = '07'
+    data = '14'
+    datas = ano+mes+data
+    imagem = get(datas)
+    caminho_arquivo = f'../APOC/{datas}.jpg'
+    verificar_caminho(caminho_arquivo)
+    if imagem is not None:
+        print(f"Arquivo {datas} encontrado!")
+    else:
+        print("Arquivo não encontrado.")
